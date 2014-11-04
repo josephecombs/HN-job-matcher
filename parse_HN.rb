80ad95f24870a31b40e61eb9df9d31d41a5de67b
Joseph Combs <joseph.e.combs@gmail.com>
2014-11-04 14:31:43 -0800

    getting email regexes working

--- parse_HN.rb
+++ parse_HN.rb
@@ -11,8 +11,33 @@ end
 
 def scrape_site
   #come back to this later
-  doc = Nokogiri::HTML(open("http://www.threescompany.com/"))  
+  doc = Nokogiri::HTML(open("https://news.ycombinator.com/item?id=8542892"))  
+  # puts doc.css('title')
+  comments = doc.css('.comment')
+  puts comments.length
+  #.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0]
+  emails_comments = {}
+  emails_comments = hashify_emails(comments)
+  puts emails_comments.keys
 end
+
+def hashify_emails(comments_arr)
+  emails_comments = {}
+  
+  comments_arr.each do |comment|
+    if comment.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0]
+      email_address = comment.match
+    end
+  end
+  
+  emails_comments
+end
+
+def test_regex
+  
+end
+
+
 
 # scrape_file
 scrape_site
