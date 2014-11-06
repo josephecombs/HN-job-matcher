d7724daf7e593c9fdb098cb80c24e327058db0ae
Joseph Combs <joseph.e.combs@gmail.com>
2014-11-05 21:21:28 -0800

    almost done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Hi everyone.  I want to use Nokogiri to parse the Hacker News jobs thread.  Next I want to match job seekers and companies who are hiring.
 
+If you posted to the "seeking" thread, you can look in /spit to see the ranking of jobs you should apply to based on .similar method's (of the similar_text gem) comparison of your post to each of the "seeking" posts.
+
 PS - plz hire me!
--- parse_HN.rb
+++ parse_HN.rb
@@ -16,8 +16,22 @@ def scrape_site(url)
   doc = Nokogiri::HTML(open(url))  
   # puts doc.css('title')
   comments = doc.css('.comment')
-  usernames = doc.css('.comhead > a').text
-  puts comments.length
+  usernames = doc.css('.comhead > a')
+  # purify usernames a bit
+  usernames = usernames.select.each_with_index { |str, i| i.even? }
+  usernames.each_with_index do |username, idx|
+    new_idx = username.to_s[0..-1].index(">")
+    
+    # puts username.to_s[17..(new_idx - 2)]
+    usernames[idx] = username.to_s[17..(new_idx - 2)]
+    # puts username.css('a')
+    # temp_username = Nokogiri::HTML(username.css('a'))
+    # puts temp_username
+    # puts temp_username.children.children.children[0].attributes["href"].value[8..-1]
+  end
+  
+  puts usernames
+  
   #.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0]
   usernames_comments = {}
   usernames_comments = hashify_comments(comments, usernames)
@@ -40,7 +54,7 @@ def hashify_comments(comments_arr, commentors_arr)
   # end
   
   comments_arr.each_with_index do |comment, idx|
-    usernames_comments[commentors_arr[idx]] = comment 
+    usernames_comments[commentors_arr[idx]] = comment.text
   end
   
   #returns hash of usernames and comment
@@ -59,14 +73,30 @@ end
 
 
 
-# hiring and seeking now in hashes where key is email address and value is comment
+# hiring and seeking now in hashes where key is username and value is comment
 hiring = scrape_site("https://news.ycombinator.com/item?id=8542892")
 seeking = scrape_site("https://news.ycombinator.com/item?id=8542898")
 
-#for each person seeking a job, score their comment relative to each hiring post
-# seeking.each_pair do |email_address, seeking_comment|
-#   hiring.each_pair do |email_address, hiring_comment|
-#     puts hiring_comment.similar(seeking_comment)
-#   end
-#   puts "analyzed one job seeker's post"
-# end
+out_file = File.new("spit/out.txt", "w")
+out_file.puts("lol")
+
+# for each person seeking a job, score their comment relative to each hiring post
+seeking.each_pair do |seeker_username, seeking_comment|
+  # keys are similarity, values are the person wanting hiring
+  score_rank = {}
+  hiring.each_pair do |hirer_username, hiring_comment|
+    score = hiring_comment.similar(seeking_comment)
+    score_rank[score] = hirer_username
+  end
+  
+  #this will actually turn score_rank into an array
+  score_rank = score_rank.sort
+  score_rank = score_rank.reverse
+  out_file = File.new("spit/" + seeker_username + ".txt", "w")
+  
+  score_rank.each do |pair|
+    out_file.puts("hiring_manager: " + pair[1] + "; similarity_score: " + pair[0].to_s[0..6])
+  end
+  
+  puts "analyzed one job seeker's post"
+end
